47ec5fb5db3897264834dae02e1629a9f251f772
Nick Haliday <nick.haliday@gmail.com>
2012-08-29 15:23:35 -0400

    Changed behavior a bit

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 )
 
 func MaxSum(in <-chan int64, out chan<- int64) {
-	var psum int64 = 0
+	var psum, maxpsum int64 = 0, 0
 	for v := range in {
 		psum += v
 		if psum < 0 {
 			psum = 0
 		}
-		out <- psum
+		if psum > maxpsum {
+			maxpsum = psum
+		}
+		out <- maxpsum
 	}
 }
 
