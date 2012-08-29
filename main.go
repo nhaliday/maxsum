472525035a8ec02b0ce63eb8ec95dd6398f4ef70
Nick Haliday <nick.haliday@gmail.com>
2012-08-29 15:03:52 -0400

    Futzing around w/ organization of Go code

--- main.go
+++ main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"io"
-	"maxsum"
 )
+
+func MaxSum(in <-chan int64, out chan<- int64) {
+	var psum int64 = 0
+	for v := range in {
+		psum += v
+		if psum < 0 {
+			psum = 0
+		}
+		out <- psum
+	}
+}
 
 func main() {
 	in := make(chan int64)
